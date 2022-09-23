ff3a13542d5c0ab44e41b8c6ef0aca82a6af84e6
MG <magallardo11@outlook.com>
2022-09-22 19:16:27 -0700

    Time working

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ const currentD = moment().format("dddd, LL");
 let currentDay = $("#currentDay");
 currentDay.text(currentD);
 
-let currentTime = moment().format("hh:mm a")
+let currentTime = moment().format("h a")
 currentDay.text(currentD)
 // console.log(currentD)
 
 let $9amInput = $('#9am')
-let $10amInput = $('10am')
-let $11amInput = $('11am')
-let $12pmInput = $('12pm')
-let $1pmInput = $('1pm')
-let $2pmInput = $('2pm')
-let $3pmInput = $('3pm')
-let $4pmInput = $('4pm')
-let $5pmInput = $('5pm')
+let $10amInput = $('#10am')
+let $11amInput = $('#11am')
+let $12pmInput = $('#12pm')
+let $1pmInput = $('#1pm')
+let $2pmInput = $('#2pm')
+let $3pmInput = $('#3pm')
+let $4pmInput = $('#4pm')
+let $5pmInput = $('#5pm')
+
+arrayof$Times = [$9amInput, $10amInput, $11amInput, $12pmInput, $1pmInput, $2pmInput, $3pmInput, $4pmInput, $5pmInput]
+
+currentDay.text(currentD)
+console.log(arrayof$Times[0])
 
 
 
+for (i=0; i<9; i++) {
+    if (moment("3 PM", 'h A').isAfter(moment("9 AM", 'h A').add(i, 'h'))) {
+        arrayof$Times[i].addClass("past");
+    } else if (moment("3 PM", 'h A').isSame(moment("9 AM", 'h A').add(i, 'h'))) {
+        arrayof$Times[i].addClass("present");
+    } else if (moment("3 PM", 'h A').isBefore(moment("9 AM", 'h A').add(i, 'h'))) {
+        arrayof$Times[i].addClass("future");
+    }
+}
 
-    moment("9:00 AM", "h:mm A").isAfter(moment("8:00 AM", "h:mm A"))
-    
-    if (moment().isAfter(moment("8:00 AM", 'h:mm A'))) {
-       $9amInput.addClass("past");}
-// if (moment().isAfter("10:00")) {
+
+
+       // if (moment().isAfter("10:00")) {
 //     $9amInput.addClass(".past");
