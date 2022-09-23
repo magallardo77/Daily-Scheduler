63aeb6e11645dc6d0366bb8fa2b2b2a0da6b575e
MG <magallardo11@outlook.com>
2022-09-22 17:38:14 -0700

    HTML/JS changes

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const currentD = moment().format("dddd, LL");
-// console.log(currentDay);
+// console.log(currentD);
 let currentDay = $("#currentDay");
 currentDay.text(currentD);
 
 let currentTime = moment.format("hh:mm a")
 currentDay.text(currentD)
+// console.log(currentD)
+
+let 9amInput = $('#9am')
+let 10amInput = $('10am')
+let 11amInput = $('11am')
+let 12pmInput = $('12pm')
+let 1pmInput = $('1pm')
+let 2pmInput = $('2pm')
+let 3pmInput = $('3pm')
+let 4pmInput = $('4pm')
+let 5pmInput = $('5pm')
