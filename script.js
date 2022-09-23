f7e618a2d032ed47e55ca1cb2d07f02b9f91b262
MG <magallardo11@outlook.com>
2022-09-22 18:18:05 -0700

    Time working

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ const currentD = moment().format("dddd, LL");
 let currentDay = $("#currentDay");
 currentDay.text(currentD);
 
-let currentTime = moment.format("hh:mm a")
+let currentTime = moment().format("hh:mm a")
 currentDay.text(currentD)
 // console.log(currentD)
 
-let 9amInput = $('#9am')
-let 10amInput = $('10am')
-let 11amInput = $('11am')
-let 12pmInput = $('12pm')
-let 1pmInput = $('1pm')
-let 2pmInput = $('2pm')
-let 3pmInput = $('3pm')
-let 4pmInput = $('4pm')
-let 5pmInput = $('5pm')
+let $9amInput = $('#9am')
+let $10amInput = $('10am')
+let $11amInput = $('11am')
+let $12pmInput = $('12pm')
+let $1pmInput = $('1pm')
+let $2pmInput = $('2pm')
+let $3pmInput = $('3pm')
+let $4pmInput = $('4pm')
+let $5pmInput = $('5pm')
+
+
+
+
+    moment("9:00 AM", "h:mm A").isAfter(moment("8:00 AM", "h:mm A"))
+    
+    if (moment().isAfter(moment("8:00 AM", 'h:mm A'))) {
+       $9amInput.addClass("past");}
+// if (moment().isAfter("10:00")) {
+//     $9amInput.addClass(".past");
