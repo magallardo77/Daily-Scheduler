6b5b7a758d73423ebe35aef22add88fa09cacbe1
MG <magallardo11@outlook.com>
2022-09-23 17:52:23 -0700

    README change

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# work-scheduler
+# Daily Scheduler
+
+## Description
+The daily scheduler was created to for users who are interested in scheduling their daily 9-5 work schedules and the site is able to save their browsing data.
+
+## Installation 
+N/A
+
+## Usage
+- Users are capable of creating a daily work schedule and store their local data.
+- Developers/students are able to draw inspiration from this site and can can view this code to examine at JavaScript functions using Jquery syntax and the use of moment().
+
+## Laungauges used
+- JavaScript
+- CSS 
+- HTML
+- Moment()
+- Jquery
+- Bootstrap
+
+## License 
+N/A
+
+## Contact Information
+- Email: magallardo11@outlook.com
--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 const currentD = moment().format("dddd, LL");
-// console.log(currentD);
 let currentDay = $("#currentDay");
 let $button = $("button")
+
 arrayof$Times = [$("#9am"), $("#10am"), $("#11am"), $("#12pm"), $("#1pm"), $("#2pm"), $("#3pm"), $("#4pm"), $("#5pm")];
+
 arrayof$TimesID = [$("#9am").attr("id"), $("#10am").attr("id"), $("#11am").attr("id"), $("#12pm").attr("id"), $("#1pm").attr("id"), $("#2pm").attr("id"), $("#3pm").attr("id"), $("#4pm").attr("id"), $("#5pm").attr("id")];
 
 currentDay.text(currentD)
-
-
-// let $9am = $('#9am')
-// let $10am = $('#10am')
-// let $11am = $('#11am')
-// let $12pm = $('#12pm')
-// let $1pm = $('#1pm')
-// let $2pm = $('#2pm')
-// let $3pm = $('#3pm')
-// let $4pm = $('#4pm')
-// let $5pm = $('#5pm')
-
-
-
-// arrayof$TimesID = [$9am, $10am, $11am, $12pm, $1pm, $2pm, $3pm, $4pm, $5pm]
-
-
-
 
 
 for (i=0; i<arrayof$Times.length; i++) {
